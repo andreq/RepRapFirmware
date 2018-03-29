e68a1172dc879180f629a2386688cdcf2d032ee4
David Crocker <dcrocker@eschertech.com>
2018-03-29 10:25:29 +0100

    More RTOS work
    IAP is now working again
    Heat module runs as a separate task

--- src/GCodes/GCodes3.cpp
+++ src/GCodes/GCodes3.cpp
@@ -732,7 +732,7 @@ GCodeResult GCodes::ProbeTool(GCodeBuffer& gb, const StringRef& reply)
 			moveBuffer.feedRate = gb.MachineState().feedrate;
 
 			// Kick off new movement
-			segmentsLeft = 1;
+			NewMoveAvailable(1);
 			gb.SetState(GCodeState::probingToolOffset);
 		}
 	}
